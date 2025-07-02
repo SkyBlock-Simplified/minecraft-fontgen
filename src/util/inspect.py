1345da7af26c12fee906ba67d10366e4e74ad621
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-07-02 00:09:51 -0400

    Working fontforge subprocess script

---
+++ src/util/inspect.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+def inspect_font_file(output_path):
+    print(f"🕵️ Inspecting font...")
+    result = subprocess.run(
+        [
+            "fontforge", "-lang=py", "-script",
+            os.path.abspath("util/validate.py"),
+            output_path
+        ],
+        encoding="utf-8", # Avoids UnicodeDecodeError
+        errors="replace", # Replaces bad chars with �
+        capture_output=True,
+        text=True
+    )
+
+    print(result.stdout)
