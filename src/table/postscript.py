4253f6c9004f2ed8eae8313436c891020721b9ca
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-07-05 05:39:50 -0400

    formatType 2.0 is bald

--- src/table/postscript.py
+++ src/table/postscript.py
@@ -3,7 +3,7 @@ from fontTools.ttLib import newTable
 def create_font_pscript_table(font, use_cff: bool = True):
     print("→ 📄 Generating postscript table...")
     post = font["post"] = newTable("post")
-    post.formatType = 3.0 if use_cff else 2.0
+    post.formatType = 3.0
     post.isFixedPitch = 0
     post.italicAngle = 0.0
     post.minMemType1 = 0
@@ -12,3 +12,5 @@ def create_font_pscript_table(font, use_cff: bool = True):
     post.maxMemType42 = 0
     post.underlinePosition = -75
     post.underlineThickness = 50
+    post.extraNames = []
+    post.mapping = {}
