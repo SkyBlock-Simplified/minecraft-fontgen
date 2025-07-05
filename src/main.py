9bf4f2a5e3b174db726bea303aecfc4613c4e81f
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-07-04 20:40:39 -0400

    Add new tile slice method

--- src/main.py
+++ src/main.py
@@ -1,24 +1,15 @@
-from file_io import read_json_file, clean_output_dir
+from file_io import read_providers_from_json_file, clean_output_dir, slice_providers_into_tiles
 from font_creator import create_font_file
 from src.util.config import OUTPUT_FILE, MINECRAFT_JAR_DEFAULT_JSON, OPENTYPE
 from src.inspect.inspect import inspect_font_file
 
 def main():
-    # Clean previous output
     clean_output_dir()
-
-    # Load providers
-    providers = read_json_file(MINECRAFT_JAR_DEFAULT_JSON)
-
-    # Create font file
+    providers = read_providers_from_json_file(MINECRAFT_JAR_DEFAULT_JSON)
+    slice_providers_into_tiles(providers)
     glyph_storage = create_font_file(providers, OPENTYPE)
-
-    # Save font file
     glyph_storage.save(OUTPUT_FILE)
-
-    # Inspect font file
     inspect_font_file(OUTPUT_FILE)
-
     print("✨ Done.")
 
 if __name__ == "__main__":
