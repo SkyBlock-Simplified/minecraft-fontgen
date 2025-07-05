0067a7e1049483836dfb5a24273a597086bb96f1
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-07-05 05:39:39 -0400

    Fix truetype table defaults

--- src/table/truetype.py
+++ src/table/truetype.py
@@ -1,15 +1,21 @@
+import numpy as np
+
 from fontTools.ttLib import newTable
+from fontTools.ttLib.tables.ttProgram import Program
 
 def create_tt_font_tables(font):
     print("→ 📄 Creating truetype table...")
     font["glyf"] = newTable("glyf")
+    font["glyf"].glyphs = {}
+
+    font["loca"] = newTable("loca") # Automatically populated
 
     print(" → ➕ Adding fontforge compatibility...")
     font["prep"] = newTable("prep") # TT instructions pre-program
-    font["prep"].program = [] # Dummy
+    font["prep"].program = Program() # Dummy
 
     font["fpgm"] = newTable("fpgm") # Font program
-    font["fpgm"].program = [] # Dummy
+    font["fpgm"].program = Program() # Dummy
 
     font["cvt "] = newTable("cvt ") # Control values
-    font["cvt "].values = [] # Dummy
+    font["cvt "].values = np.zeros(0, dtype=np.int16) # Dummy
