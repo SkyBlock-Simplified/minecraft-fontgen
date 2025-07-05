9db0d37b1a0d2cdb529e1a55eb4a32e294da0a84
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-07-05 06:01:44 -0400

    Merge constants into config

--- src/config.py
+++ src/config.py
@@ -1,13 +1,32 @@
-from src.util.constants import FONT_NAME
+from math import atan
 
-MINECRAFT_JAR_FONT_DIR = "font"
-MINECRAFT_JAR_FONT_JSON_DIR = MINECRAFT_JAR_FONT_DIR + "/" + "include"
-MINECRAFT_JAR_DEFAULT_JSON = MINECRAFT_JAR_FONT_JSON_DIR + "/" + "default.json"
-
+# Font
+AUTHOR = "CraftedFury"
+FONT_NAME = "Minecraft"
+VERSION = "1.1.0"
+MAC_EPOCH = 2082844800 # FontTools requires seconds since 12:00 midnight, January 1, 1904 UTC
 OPENTYPE = True
 CREATE_BMP = True
 CREATE_SMP = True
 
-SVG_WIDTH = 256
+# File Input/Output
+MOJANG_MANIFEST_URL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
 OUTPUT_DIR = "output"
-OUTPUT_FILE = OUTPUT_DIR + "/" + FONT_NAME + (".otf" if OPENTYPE else ".ttf")
+OUTPUT_JAR_DIR = OUTPUT_DIR + "/" + "assets"
+OUTPUT_FONT_FILE = OUTPUT_DIR + "/" + FONT_NAME + (".otf" if OPENTYPE else ".ttf")
+
+# Glyph
+COLUMNS_PER_ROW = 16
+DEFAULT_GLYPH_SIZE = 8
+BOUNDING_BOX = [0, -128, 1152, 896]
+UNITS_PER_EM = 1024
+ASCENT = BOUNDING_BOX[3]
+DESCENT = BOUNDING_BOX[1]
+ADVANCE_WIDTH = BOUNDING_BOX[2] + BOUNDING_BOX[1]
+NOTDEF = ".notdef"
+NOTDEF_GLYPH = [20, 0, 437, 675]
+
+# Italic Glyph
+ITALIC_SHEAR_VERTICAL = 5
+ITALIC_SHEAR_FACTOR = 1 / ITALIC_SHEAR_VERTICAL
+ITALIC_SHEAR_ANGLE = atan(ITALIC_SHEAR_FACTOR)
