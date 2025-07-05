c3a4b0ff29693ab3c2d896ebee5865475d9ba15a
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-07-05 07:19:58 -0400

    Cleanup minecraft constants

--- src/config.py
+++ src/config.py
@@ -12,10 +12,10 @@ CREATE_SMP = True # SMP (Format 12) (U+10000 - U+1FFFF)
 # === CONSTANTS / DO NOT EDIT ===
 
 # File Input/Output
-MOJANG_MANIFEST_URL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
 OUTPUT_DIR = "output"
-OUTPUT_JAR_DIR = OUTPUT_DIR + "/" + "assets"
-OUTPUT_FONT_FILE = OUTPUT_DIR + "/" + FONT_NAME + (".otf" if OPENTYPE else ".ttf")
+OUTPUT_FONT_FILE = f"{OUTPUT_DIR}/{FONT_NAME}" + (".otf" if OPENTYPE else ".ttf")
+MOJANG_MANIFEST_URL = "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"
+MINECRAFT_JAR_DIR = OUTPUT_DIR + "/assets/minecraft"
 
 # Glyph
 COLUMNS_PER_ROW = 16
