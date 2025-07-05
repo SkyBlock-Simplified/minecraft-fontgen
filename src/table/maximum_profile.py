00a0148e06d239f85fa5fd48513eb8c44c85676d
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-07-05 05:41:14 -0400

    Add missing TrueType defaults for the maxp table

--- src/table/maximum_profile.py
+++ src/table/maximum_profile.py
@@ -7,3 +7,15 @@ def create_font_mprofile_table(font, use_cff: bool = True):
     maxp = font["maxp"] = newTable("maxp")
     maxp.tableVersion = 0x00005000 if use_cff else 0x00010000
     maxp.maxPoints = DEFAULT_GLYPH_SIZE * DEFAULT_GLYPH_SIZE
+    maxp.maxContours = 0
+    maxp.maxCompositePoints = 0
+    maxp.maxCompositeContours = 0
+    maxp.maxZones = 2
+    maxp.maxTwilightPoints = 0
+    maxp.maxStorage = 0
+    maxp.maxFunctionDefs = 0
+    maxp.maxInstructionDefs = 0
+    maxp.maxStackElements = 0
+    maxp.maxSizeOfInstructions = 0
+    maxp.maxComponentElements = 0
+    maxp.maxComponentDepth = 0
