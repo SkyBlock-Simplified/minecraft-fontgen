4af1bd2013849653861c11a3319cb85d3f3fd51e
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-07-05 05:40:36 -0400

    fontRevision is just a float, tableVersion on truetype has a stroke with the intended value

--- src/table/header.py
+++ src/table/header.py
@@ -10,7 +10,7 @@ def create_font_header_table(font, use_cff: bool = True):
     head.checkSumAdjustment = 0 # Used to ensure the font has a valid file checksum (recalculated automatically)
     head.created = now + (MAC_EPOCH - int(time.mktime(time.gmtime(0)))) # Creation timestamp of the font (unix timestamp)
     head.flags = 0x03 # Bit flags that define font-wide behavior (e.g., baseline, left sidebearing point at x=0)
-    head.fontRevision = 0x00005000 if use_cff else 0x00010000
+    head.fontRevision = 1.0
     head.fontDirectionHint = 2 # Used by font renderers for direction hints (usually set to 2 for modern fonts)
     head.glyphDataFormat = 0
     head.indexToLocFormat = 0 if not use_cff else 1 # 0 = short offsets (16-bit), 1 = long offsets (32-bit) in the loca table
@@ -18,6 +18,6 @@ def create_font_header_table(font, use_cff: bool = True):
     head.macStyle = 0 # Bit flags for font styling (e.g., bold, italic)
     head.magicNumber = 0x5F0F3CF5 # Verification signature for OpenType and TrueType
     head.modified = now + (MAC_EPOCH - int(time.mktime(time.gmtime(0)))) # Last modified timestamp of the font
-    head.tableVersion = 0x00005000 if use_cff else 0x00010000
+    head.tableVersion = 0x00005000 if use_cff else 0x00001000
     head.unitsPerEm = UNITS_PER_EM # Defines the em square size (Higher values increase the resolution of glyph coordinates)
     head.xMin, head.yMin, head.xMax, head.yMax = BOUNDING_BOX # Bounding Box of all glyphs
